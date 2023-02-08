3c04cc99c106f0b993026de1f50b7bb21d625acd
Lyod <mpaproject307@gmail.com>
2023-02-08 21:27:21 +0700

    EmployeRequest

---
+++ app/Http/Requests/EmployeRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\Rules\Enum;
+
+class EmployeRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'fullname' => 'required|string',
+            'username' => 'required|string',
+            'password' => 'required',
+            'address' => 'required|string|min:3',
+            'gender' => 'required',
+            'birth_date' => 'required|date',
+            'salary' => 'required|integer'
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'message' => 'Validation Error',
+            'errors' => $validator->errors()
+        ]));
+    }
+}
