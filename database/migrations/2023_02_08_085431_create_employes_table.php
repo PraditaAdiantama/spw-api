14b75d43bafffca31117bae3986af5cb82d45621
Lyod <mpaproject307@gmail.com>
2023-02-08 20:48:59 +0700

    Add Employe model and make Get method

---
+++ database/migrations/2023_02_08_085431_create_employes_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employes', function (Blueprint $table) {
+            $table->id();
+            $table->string('fullname');
+            $table->string('username');
+            $table->string('password');
+            $table->string('address');
+            $table->enum('gender',[
+                'male',
+                'female'
+            ]);
+            $table->date('birth_date');
+            $table->double('salary');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employes');
+    }
+};
